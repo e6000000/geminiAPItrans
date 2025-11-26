8a8dac6e8b650366e4688590d2f03aa4c627b35b
e6000000 <com24ed@gmail.com>
2025-11-27 00:06:51 +0100

    Auto-Commit 2025-11-27  0:06:43,24 see comments in file OLDCODE: NEWCODE:

---
+++ EDsrc/main/2748/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+// Ersetze den Text in den Anführungszeichen mit deinem echten Google API Key  AIzaSyD2AHThvr9NF1cu9MQ5kfboHWTBA42h9UY
+// const CONFIG_API_KEY = "AIzaSyAREpxEF00zy49XoxSNtmRJGY7w9seso-E";
+const CONFIG_API_KEY = "AIzaSyD2AHThvr9NF1cu9MQ5kfboHWTBA42h9UY";
+// Optional: Wähle eine Standard-Stimme (Puck, Charon, Kore, Fenrir, Aoede)
+const CONFIG_VOICE = "Puck";
+
+
+// 2. Standard-Modell (optional, wird im Dropdown vorausgewählt)
+const CONFIG_MODEL = "models/gemini-2.0-flash-exp"; 
+
